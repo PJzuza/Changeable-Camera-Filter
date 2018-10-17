dd7afa309ff8397c2bb813e6c9bec7e633cef0f7
PJzuza <PJzuza@gmail.com>
2018-10-18 02:32:13 +0700

    Prepared auto-updater once again
    - Updated updates.lua
    - Changed Shockwave to Dom (His current name)
    - Updated README.md

--- lua/updates.lua
+++ lua/updates.lua
@@ -2,7 +2,9 @@ if UpdateThisMod then
 	UpdateThisMod:Add({
 		mod_id = 'Changeable Camera Filter',
 		data = {
-			modworkshop_id = 22650
+			modworkshop_id = 22650,
+			dl_url = "https://github.com/PJzuza/Changeable-Camera-Filter/raw/master/updates/CCF.zip",
+			info_url = "https://raw.githubusercontent.com/PJzuza/Changeable-Camera-Filter/master/mod.txt"
 		}
 	})
 end

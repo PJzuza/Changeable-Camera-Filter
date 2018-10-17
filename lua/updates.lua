aa17ffd44c39df5f985efd282256666a61ca2ee9
shinrax2 <shinra.ubu1@yahoo.de>
2018-10-17 16:00:28 +0200

    fix auto-update

--- lua/updates.lua
+++ lua/updates.lua
@@ -1,19 +1,8 @@
-local superblt = false
-for _, mod in pairs(BLT and BLT.Mods:Mods() or {}) do
-	if mod:GetName() == "SuperBLT" and mod:IsEnabled() then
-		superblt = true
-		break
-	end			
-end
-if superblt == false then
-	if UpdateThisMod then
-		UpdateThisMod:Add({
-			mod_id = 'Changeable Camera Filter',
-			data = {
-				dl_url = 'https://raw.githubusercontent.com/PJzuza/Changeable-Camera-Filter/blob/master/updates/CCF.zip',
-				info_url = 'https://raw.githubusercontent.com/PJzuza/Changeable-Camera-Filter/master/mod.txt'
-			}
-		})
-	end
-
+if UpdateThisMod then
+	UpdateThisMod:Add({
+		mod_id = 'Changeable Camera Filter',
+		data = {
+			modworkshop_id = 22650
+		}
+	})
 end

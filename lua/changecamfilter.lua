64e7c41174b185796a646dd1a4f1643f51a8891c
Dominik Vyhňák <38251670+dominikvyhnak@users.noreply.github.com>
2018-10-18 15:02:44 +0200

    Fixed changecamfilter.lua
    Fixed a crash
    Improved code a bit

--- lua/changecamfilter.lua
+++ lua/changecamfilter.lua
@@ -1,51 +1,52 @@
-	_G.CCF = _G.CCF or {}
-	CCF._path = ModPath
-	CCF.settings_path = SavePath .. "CCF.txt"
-	CCF.settings = {}
-	CCF.Choose_Camera_Filter = 	
-		{
-			"color_sin",
-			"color_main",
-			"color_payday",
-			"color_heat",
-			"color_nice",
-			"color_bhd",
-			"color_xgen",
-			"color_xxxgen",
-			"color_matrix",
-			"color_matrix_classic",
-			"color_sin_classic",
-			"color_sepia",
-			"color_sunsetstrip",
-			"color_colorful",
-			"color_madplanet"
-		}
+_G.CCF = _G.CCF or
+{
+	_path = ModPath,
+	settings_path = SavePath .. "CCF.txt",
+	Choose_Camera_Filter = 	
+	{
+		"color_sin",
+		"color_main",
+		"color_payday",
+		"color_heat",
+		"color_nice",
+		"color_bhd",
+		"color_xgen",
+		"color_xxxgen",
+		"color_matrix",
+		"color_matrix_classic",
+		"color_sin_classic",
+		"color_sepia",
+		"color_sunsetstrip",
+		"color_colorful",
+		"color_madplanet"
+    }
+}
 -- Store Color Gradient before using it as a string value in PostHook
 function CCF:GetFilter()
 	return self.Choose_Camera_Filter[self.settings.color_camera_filters_value]
 end
 
-function CCF:Reset()
+function CCF:LoadDefaultSettings()
 	self.settings = {
 		color_camera_filters_value = 1,
-		ccf_options_removecameranoise_toggle_value = false,
+		ccf_options_removecameranoise_toggle_value = false
 	}
 end
 
 function CCF:Save()
-	local file = io.open(CCF.settings_path, "w+")
+	local file = io.open(self.settings_path, "w+")
 	if file then
-		file:write(json.encode(CCF.settings))
+		file:write(json.encode(self.settings))
 		file:close()
 	end
 end
 
 function CCF:Load()
-	self:Reset()
+	self:LoadDefaultSettings()
 	local file = io.open(self.settings_path, "r")
 	if file then
 		for k, v in pairs(json.decode(file:read('*all')) or {}) do
-				self.settings[k] = v
+			self.settings[k] = v
 		end
 		file:close()
 	end
@@ -53,7 +54,7 @@ end
 
 --PostHook to overide a function IngameAccessCamera:at_enter
 if RequiredScript == "lib/states/ingameaccesscamera" then
-	Hooks:PostHook( IngameAccessCamera, "at_enter" , "ApplyCCF" , function(self)
+	Hooks:PostHook(IngameAccessCamera, "at_enter" , "ApplyCCF" , function(self)
 		managers.environment_controller:set_default_color_grading(CCF:GetFilter(), true)
 		managers.environment_controller:refresh_render_settings()
 	end)
@@ -62,9 +63,8 @@ end
 --PostHook to remove noise from camera
 --thx to Cpone for his help with this
 if RequiredScript == "lib/managers/hud/hudaccesscamera" then
-	Hooks:PostHook( HUDAccessCamera, "init", "HideHUDNoiseCCF", 
-	function(self)
-		if CCF.settings.ccf_options_removecameranoise_toggle_value == true then
+	Hooks:PostHook(HUDAccessCamera, "init", "HideHUDNoiseCCF", function(self)
+		if CCF.settings.ccf_options_removecameranoise_toggle_value then
 			self._full_hud_panel:child("noise"):set_visible(false)
 			self._full_hud_panel:child("noise2"):set_visible(false)
 		end
@@ -85,10 +85,10 @@ Hooks:Add("LocalizationManagerPostInit", "LocalizationManagerPostInit_CCF", func
 			end			
 		end
 		if custom_language then
-			loc:load_localization_file(CCF._path .. "loc/" .. custom_language ..".txt")
+			loc:load_localization_file(CCF._path .. "loc/" .. custom_language ..".json")
 		else
 			for _, filename in pairs(file.GetFiles(CCF._path .. "loc/")) do
-				local str = filename:match('^(.*).txt$')
+				local str = filename:match('^(.*).json$')
 				if str and Idstring(str) and Idstring(str):key() == SystemInfo:language():key() then
 					loc:load_localization_file(CCF._path .. "loc/" .. filename)
 					break
@@ -96,17 +96,17 @@ Hooks:Add("LocalizationManagerPostInit", "LocalizationManagerPostInit_CCF", func
 			end
 		end
 	end
-	loc:load_localization_file(CCF._path .. "loc/english.txt", false)
+	loc:load_localization_file(CCF._path .. "loc/english.json", false)
 end)
 
--- Menu stuffs
-Hooks:Add( "MenuManagerInitialize", "MenuManagerInitialize_CCF", function( menu_manager )
+-- Menu stuff
+Hooks:Add("MenuManagerInitialize", "MenuManagerInitialize_CCF", function(menu_manager)
 	MenuCallbackHandler.callback_color_camera_filters = function (self, item)
 		CCF.settings.color_camera_filters_value = item:value()
-	end
+    end
+    
 	MenuCallbackHandler.callback_ccf_options_removecameranoise_enabled = function(self, item)
-		CCF.settings.ccf_options_removecameranoise_toggle_value = (item:value() == "on" and true or false)
-		CCF:Save()
+		CCF.settings.ccf_options_removecameranoise_toggle_value = item:value() == "on"
 	end
 	
 	MenuCallbackHandler.ccf_save = function(this, item)
@@ -115,9 +115,9 @@ Hooks:Add( "MenuManagerInitialize", "MenuManagerInitialize_CCF", function( menu_
 	
 	MenuCallbackHandler.callback_ccf_reset = function(self, item)
 		MenuHelper:ResetItemsToDefaultValue(item, {["color_camera_filters"] = true}, 1)
-		MenuHelper:ResetItemsToDefaultValue(item, {["ccf_options_removecameranoise_enabled_toggle"] = true}, ccf_options_removecameranoise_toggle_value)
+		MenuHelper:ResetItemsToDefaultValue(item, {["ccf_options_removecameranoise_enabled_toggle"] = true}, false)
 	end
 	
 	CCF:Load()	
-	MenuHelper:LoadFromJsonFile(CCF._path .. "menu/options.txt", CCF, CCF.settings)
-end )
+	MenuHelper:LoadFromJsonFile(CCF._path .. "menu/options.json", CCF, CCF.settings)
+end)
